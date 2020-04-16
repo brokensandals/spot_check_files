d4f88a60311ddf98c530b36b20141b96cc3e333d
Jacob Williams <jacobaw@gmail.com>
2020-04-16 09:48:28 -0700

    add --thumbnails argument

--- src/spot_check_files/cli.py
+++ src/spot_check_files/cli.py
@@ -12,6 +12,7 @@ def _print_images(checker):
     for (vpath, thumbnail) in checker.thumbnails:
         print(f'thumbnail for {vpath}:')
         imgcat(thumbnail)
+        print()
 
 
 def main(args=None):
@@ -19,9 +20,13 @@ def main(args=None):
     parser.add_argument(
         'path', nargs='+',
         help='files or folders to check')
+    parser.add_argument(
+        '-t', '--thumbnails', nargs='?', type=int,
+        help='maximum number of thumbnails to generate')
+    parser.set_defaults(thumbnails=3)
     args = parser.parse_args(args)
 
-    checker = Checker()
+    checker = Checker(num_thumbnails=args.thumbnails)
     for path in args.path:
         checker.check(path)
 
@@ -29,6 +34,7 @@ def main(args=None):
     for vpath in checker.problems:
         for problem in checker.problems[vpath]:
             print(f'WARNING {vpath}: {problem}')
+    print()
 
     if os.environ.get('TERM_PROGRAM', None) == 'iTerm.app':
         _print_images(checker)
