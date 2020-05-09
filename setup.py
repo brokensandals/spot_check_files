6ea477e51f48dcd0aa2932ff48ac430dbbc7f26b
Jacob Williams <jacobaw@gmail.com>
2020-05-09 09:57:52 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spot_check_files",
-    version="0.0.2",
+    version="0.0.3",
     author="Jacob Williams",
     author_email="jacobaw@gmail.com",
     description="Helps validate the integrity of data backups/exports.",
